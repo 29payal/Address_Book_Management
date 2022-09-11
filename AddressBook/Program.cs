e7a3ba2f335796179b7f08739f45a3aa113dc043
Payal Sharma <https://github.com/29payal>
2022-09-11 21:14:39 +0530

    [Payal] UC1-Contacts

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -11,6 +11,19 @@ namespace AddressBook
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Address Book Program! ");
+            Console.Write("Enter First Name, Last Name, Address, City, State, Zip, Phone Number, Email \n");
+            Contacts addressBook = new Contacts()
+            {
+                FirstName = Console.ReadLine(),
+                LastName = Console.ReadLine(),
+                Address = Console.ReadLine(),
+                City = Console.ReadLine(),
+                State = Console.ReadLine(),
+                Zip = Convert.ToDouble(Console.ReadLine()),
+                PhoneNumber = Convert.ToDouble(Console.ReadLine()),
+                Email = Console.ReadLine(),
+            };
+            addressBook.DisplayContact();
             Console.ReadLine();
         }
     }
